98440b8c584531cf76b6cbf9a0bcaac3c6ebea51
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-10 20:42:44 -0500

    updated collab file

--- collabTest.js
+++ collabTest.js
@@ -1,1 +1,14 @@
 // Collaboration in GitHub.
+
+
+// 1. Create a FORK of the repository that you want to work in.
+// 2. Then CLONE that fork
+        //! git clone <forked repo location>
+
+// 3. Now lets set your remote to the main repo that you created a fork from.
+        //! git remote add upstream <main repo-location>
+
+// 4. Now you can update your fork with all of the changes made in th emain repo. Navigate to the main branch of your repo before you do this.
+        //! git checkout main
+        //! git fetch upstream
+        //! git rebase upstream/main
