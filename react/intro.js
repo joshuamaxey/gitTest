929378f86b47a0367768313902379a7011b29552
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-07-21 15:57:29 -0500

    changes

--- react/intro.js
+++ react/intro.js
@@ -1,12 +1,13 @@
-//! Here is the linke to the video I'm using for independently studying react:
+//! Here is the link to the video I'm using for independently studying react:
 
 //^ https://www.youtube.com/watch?v=4UZrsTqkcW4
 
 //! Setting Up Dev Environment:
 
 //^ 1. Node (min version 5.2.0)
-    // Ran "node --version" in Ubuntu terminal, returned "v18.20.0"
-    // Node is installed and up to date.
+    // node version 18.20.0
+    // npm version 10.5.2
+    // Installed and up to date.
 
 //^ 2. Browser (Chrome)
     // Installed and up to date
@@ -14,5 +15,16 @@
 //^ 3. Text Editor (Visual Studio Code)
     // Installed and up to date
 
-//^ 4. React Developer Tools
-    //
+//^ 4. React Developer Tools (Chrome Extension)
+    // Installed and up to date
+
+//! Basic Terminal Commands:
+
+// pwd - full pathname to current working directory
+// ls - list
+// ls -a - list (with hidden files/directories)
+// mkdir - create new directory
+// cd (..) - change directory
+// clear - clear console
+// ctrl + c / d - end code execution
+// arrow up / down - previous commands
