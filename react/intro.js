0bbdb3fff896ab2d920c3ddaddf7fd13acaddc4e
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-07-21 15:25:26 -0500

    Began indepenent react study

---
+++ react/intro.js
@@ -0,0 +1,18 @@
+//! Here is the linke to the video I'm using for independently studying react:
+
+//^ https://www.youtube.com/watch?v=4UZrsTqkcW4
+
+//! Setting Up Dev Environment:
+
+//^ 1. Node (min version 5.2.0)
+    // Ran "node --version" in Ubuntu terminal, returned "v18.20.0"
+    // Node is installed and up to date.
+
+//^ 2. Browser (Chrome)
+    // Installed and up to date
+
+//^ 3. Text Editor (Visual Studio Code)
+    // Installed and up to date
+
+//^ 4. React Developer Tools
+    //
